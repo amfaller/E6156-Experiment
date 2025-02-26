7006a8c6abcddbdc5cd932ff17c1c365a9ecb1b2
Tony Faller <af3370@columbia.edu>
2025-02-26 15:15:45 -0500

    Check procfs on every instance

--- driver/load.sh
+++ driver/load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROC_FILE="/proc/ScanResults"
+
 # Get the absolute path of this script
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -7,9 +9,20 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 MODULE_PATH="$SCRIPT_DIR/malwareDriver.ko"
 sudo insmod $MODULE_PATH
 
-# TODO: Launch snake or something
+
+# Check the results of the "system" scan
+if grep -q "Found target file" ${PROC_FILE}; then
+    echo "Found known cheats; not launching game!"
+else
+    echo "Launching program..."
+    firefox
+fi
 
 # Clean up (remove driver)
 sudo rmmod $MODULE_PATH
+
+# TODO DELETE
+read -p "Press any key" name
+echo "Goodbye $name!"
 
 
